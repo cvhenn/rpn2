c4a7d654a8c648d705fd7adf1e132cec00c21848
cvhenn <cvhenn@umich.edu>
2019-11-04 21:48:11 -0500

    rpn functions with output

--- rpn.py
+++ rpn.py
@@ -2,6 +2,7 @@
 
 import operator
 
+
 operators = {
     '+': operator.add,
     '-': operator.sub,
@@ -13,24 +14,24 @@ def calculate(myarg):
     stack = list()
     for token in myarg.split():
         try:
-	    token = int(token)
-	    stack.append(token)
-	except ValueError:
-	    function = operators[token]
-	    arg2 = stack.pop()
-	    arg1 = stack.pop()
-	    result = function(arg1, arg2)
-	    stack.append(result)
-	print(stack)
-	
-     if len(stack) != 1:
-	raise TypeError("Too many parameters")
-     return stack.pop()
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
     while True:
-        result =  calculate(input("rpn calc> "))
-	print("Result: ", result)
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
+
